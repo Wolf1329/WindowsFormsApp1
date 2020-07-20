438ce4fbc791eadd9a4b2ac29f6f352f16d9301d
谢学典 <1138670400@qq.com>
2020-07-20 15:04:10 +0800

    2020-7-20 15:04:09

--- BUILD.ps1
+++ BUILD.ps1
@@ -16,14 +16,15 @@ function Build-NetFrameworkx64
 
 	$outdir = "$net_baseoutput\x64"
 
-	msbuild -v:m -m -t:Build /p:Configuration="Release" /p:Platform="x64" /p:TargetFramework=net48 /p:Runtimeidentifier=win-x64 /restore
+	#msbuild -v:m -m -t:Build /p:Configuration="Release" /p:Platform="x64" /p:TargetFramework=net40 /p:Runtimeidentifier=win-x64 /restore
+	sbuild -v:m -m -t:Build /p:Configuration="Release" /p:Platform="x64" /p:TargetFramework=net40 /restore
 	if ($LASTEXITCODE) { cd $mainDir ; exit $LASTEXITCODE } 
 
     Write-Host 'Build x64 Completed, start copy bin, mode, i18n file'
-	Remove-Item -Recurse -Force "$net_baseoutput\x64\Release\win-x64\bin\tap-driver"
-	Copy-Item -Recurse "$mainDir\binaries\*" "$net_baseoutput\x64\Release\win-x64\bin"
-	Copy-Item -Recurse "$mainDir\modes\mode\*" "$net_baseoutput\x64\Release\win-x64\mode"
-	Copy-Item -Recurse "$mainDir\translations\i18n\*" "$net_baseoutput\x64\Release\win-x64\i18n"
+	#Remove-Item -Recurse -Force "$net_baseoutput\x64\Release\win-x64\bin\tap-driver"
+	#Copy-Item -Recurse "$mainDir\binaries\*" "$net_baseoutput\x64\Release\win-x64\bin"
+	#Copy-Item -Recurse "$mainDir\modes\mode\*" "$net_baseoutput\x64\Release\win-x64\mode"
+	#Copy-Item -Recurse "$mainDir\translations\i18n\*" "$net_baseoutput\x64\Release\win-x64\i18n"
 
     Write-Host 'Build done'
 }
